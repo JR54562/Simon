c9a73be46e4c0d6cc2ea4571fdf375c85fe8c538
Jay Reische <ReischeJay@JohnDeere.com>
2021-09-17 14:09:41 -0500

    setting up flashing colors

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-const bottYellow = document.querySelector(".bottomYellow")
-const topGreen = document.querySelector(".topGreen")
-const topRed = document.querySelector(".topRed")
-const bottBlue = document.querySelector(".bottomBlue")
-const resetGame = document.querySelector(".reset >startGame")
+const bottYellow = document.querySelector(".bottomYellow");
+const topGreen = document.querySelector(".topGreen");
+const topRed = document.querySelector(".topRed");
+const bottBlue = document.querySelector(".bottomBlue");
+const resetGame = document.querySelector(".reset >startGame");
 
-const arrShown = [
-    topGreen,
-    topRed,
-    bottYellow,
-    bottBlue,
-]
+const arrShown = [topGreen, topRed, bottYellow, bottBlue];
 // console.log(arrShown[2])
 // console.log(bottYellow)
 
-const flash = panel => {
-    return new Promise((resolve, reject) => {
-        piece.className = piece.className.replace(
-            'active',
-            ''
-        );
-        resolve();
-    }, 1000);
+function flashBG() {
+    var panels = document.getElementsByClassName('panel');
+    for(i = 0; i < panels.length; i++) {
+      panels[i].style.backgroundColor = 'white';
+    }
 }
+//  flashBG()

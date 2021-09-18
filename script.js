762fee67a429ca04b68fdf462c93c740e25480f9
Jay Reische <ReischeJay@JohnDeere.com>
2021-09-17 21:11:12 -0500

    committing for the night

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-const bottYellow = document.querySelector(".bottomYellow");
-const topGreen = document.querySelector(".topGreen");
-const topRed = document.querySelector(".topRed");
-const bottBlue = document.querySelector(".bottomBlue");
+const yellow = document.querySelector("#yellow");
+const green = document.querySelector("#green");
+const red = document.querySelector("#red");
+const blue = document.querySelector("#blue");
 const resetGame = document.querySelector(".reset >startGame");
 
-const arrShown = [topGreen, topRed, bottYellow, bottBlue];
-// console.log(arrShown[2])
-// console.log(bottYellow)
+const sequence = [green, red, yellow, blue];
 
+const colors = ["green", "red", "yellow", "blue"];
+let userArray = [];
+let compArray = [];
 let panels = document.querySelectorAll(".panel");
 
-function flashBG() {
-    console.log(panels);
-    for (let i = 0; i < panels.length; i++) {
-        let originalColor = getComputedStyle(panels[i]).backgroundColor;
-    //    console.log(originalColor[i])
-        panels[i].style.backgroundColor = 'white';
-    };
+// have Simon start building array to be played
+function addCompColor() {
+  let x = Math.floor(Math.random() * 4);
+  compArray.push(x);
+}
+addCompColor();
+//console.log(compArray)
+
+  // start the round and show the first sequence
+function gameTurn() {
+    // game will show a sequence to use to repeat
+}
+
+// adding event listeners for "panel" click
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", panelClick);
+});
+
+// let counter = 1
+
+const panelClick = (event) => {
+  // Add click functionality to every 'panel'
 };
 
-setTimeout(flashBG, 1000)
+var div = document.getElementsById("panel");

af191e3d096d1f88f46732e1a5dfa4464fbf8a80
Jay Reische <ReischeJay@JohnDeere.com>
2021-09-19 19:03:52 -0500

    working on functions

--- script.js
+++ script.js
@@ -2,61 +2,86 @@ const yellow = document.querySelector("#yellow");
 const green = document.querySelector("#green");
 const red = document.querySelector("#red");
 const blue = document.querySelector("#blue");
-const resetGame = document.querySelector(".reset >startGame");
-
+const resetGame = document.querySelector("#startGame");
+const panels = document.querySelectorAll(".panel");
+const round = document.querySelector("#round");
 const sequence = [green, red, yellow, blue];
 
 const colors = ["green", "red", "yellow", "blue"];
 let userArray = [];
-let compArray = [];
-let panels = document.querySelectorAll(".panel");
+let compArray = [0,2,3,1];
+let level = 1;
 
-// have Simon start building array to be played
-function addCompColor() {
-  let x = Math.floor(Math.random() * 4);
-  compArray.push(x);
+resetGame.addEventListener("click", function(){
+    round.innerText = level;
+});
+
+// Game start
+function startGame() {
+//  resetGame.classList.add("hidden");
 }
-
 // ~Change rounds~
 function rndChange() {
     addCompColor();
-}
-// ~End Change rounds~
-console.log(compArray)
+} // ~This adds new color added to compArray~
+
+console.log(colors[1]);
+
+// have Simon start building array to be played
+function addCompColor() {
+    let x = Math.floor(Math.random() * 4);
+    compArray.push(x);
+}  // compArray now holds a new value
+
+
+// for each index in compArray, flash the same index in 'sequence'
+  // - change backgroundColor to "white" for 1 sec timer (add class)
+  // change back to default value (remove class)
 
 
 function flash() {
-    // for each index in compArray, show the value of colorArray
-    // - store the current background style into var
-    // - change backgroundColor to "white" for 1 sec timer
-    // change back to variable store value
-}
+    for (let i = 0; i < compArray.length; i++){
+        console.log(compArray[i]);
+        let x = i;
+        console.log(x)
+        sequence[x].classList.add("flash");;
+        setTimeout(function () {            
+            console.log(compArray[i]);
+            console.log(sequence[x]);
+            sequence[x].classList.remove("flash");
+               }, 1000);
+    };
+    flash(sequence[x]); 
+};
 
-  // start the round and show the first sequence
+//flash()
+
+
+// start the round and show the first sequence
 function gameTurn() {
-    // game will show a sequence (value of compArray in colorsArray) For loop
-    // func Flash() will be called for each panel in compArray    
+  // game will show a sequence (value of compArray in colorsArray) For loop
+  // func Flash() will be called for each panel in compArray
 }
 
 function plyrTurn() {
-    // adding event listeners for "panel" click
-    panels.forEach((panel) => {
-        panel.addEventListener("click", panelClick);
-    });
-    // Add click functionality to every 'panel'
-const panelClick = (event) => {    
-    // Store value of index of panel selected in plyrArray
-    // compare plyrArray to compArray; if true - rndChange()
-    // Else alert to failure
+  // adding event listeners for "panel" click
+    for (var i = 0; i < panels.length; i++){
+panels[i].addEventListener("click", panelClick);
+    }
+    function panelClick(i) {
+        userArray.push[i]
+    }      
 };
-    
-}
+  
+plyrTurn()
+console.log(userArray)
 
+
+      // Add click functionality to every 'panel'
+//   const panelClick = (event) => {
+    // Store value of index of panel selected in userArray
+    // compare userArray to compArray; if true - rndChange()
+    // Else alert to failure
   
 
-
-// let counter = 1
-
-
-
-
+// let counter = 1;
